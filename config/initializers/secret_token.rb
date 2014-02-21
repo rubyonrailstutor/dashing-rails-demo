c9e1f57a791c5ea6dd056b643df249eab426cd31
John Davison <johncdavison@gmail.com>
2014-02-21 14:24:07 -0800

    WIP II

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dashing::Application.config.secret_key_base = '445ca059ffa4cdc47945144f1b1af7321da95404f966f12051cc84b09e32409f76836e04a705cec0cdbe920d41a7b6d6845bdcc1b7de8f63886f0cf1cfeb82a0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dashing::Application.config.session_store :cookie_store, key: '_dashing_session'
